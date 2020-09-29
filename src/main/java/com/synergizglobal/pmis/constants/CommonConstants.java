dfd73c1c33bf991cc9fd144f12cdaed690720027
nihalswarna <nihal.swarna@synergizglobal.com>
2020-09-29 09:00:51 +0530

    file paths

--- src/main/java/com/synergizglobal/pmis/constants/CommonConstants.java
+++ src/main/java/com/synergizglobal/pmis/constants/CommonConstants.java
@@ -18,14 +18,13 @@ public class CommonConstants {
 	public final static String LOGIN_BACKGROUND_IMAGE = "E:"+File.separator+"Development"+File.separator+"nginx-1.9.9"+File.separator+"html"+File.separator+"pmis"+File.separator+"resources"+File.separator+"images"+File.separator;
 	public static String LOGIN_BACKGROUND_IMAGE_URL = "/pmis/resources/images/login-background.jpg";
 	public final static String CONTEXT_PATH = "http://localhost/pmis";
-	
-	
 	public final static String STRIPCHART_FILE_SAVING_PATH = "E:"+File.separator+"Development"+File.separator+"nginx-1.9.9"+File.separator+"html"+File.separator+"pmis"+File.separator+"STRIPCHART_FILES"+File.separator;
 	
 	/*********** PRODUCTION ****************************************************/
 	
 	/*public final static String LOGIN_BACKGROUND_IMAGE = "usr/share/nginx/html/pmis/resources/images/";
 	public static String LOGIN_BACKGROUND_IMAGE_URL = "/pmis/resources/images/login-background.jpg";
-	public final static String CONTEXT_PATH = "http://syntrackpro.com/pmis";*/
+	public final static String CONTEXT_PATH = "http://syntrackpro.com/pmis";
+	public final static String STRIPCHART_FILE_SAVING_PATH = "usr/share/nginx/html/pmis/STRIPCHART_FILES/";*/
 	
 }
--- src/main/java/com/synergizglobal/pmis/constants/CommonConstants2.java
+++ src/main/java/com/synergizglobal/pmis/constants/CommonConstants2.java
@@ -3,6 +3,13 @@ package com.synergizglobal.pmis.constants;
 import java.io.File;
 
 public class CommonConstants2 {
+	/*********** LOCAL ****************************************************/
+	
 	public final static String ISSUE_FILE_SAVING_PATH = "E:"+File.separator+"Development"+File.separator+"nginx-1.9.9"+File.separator+"html"+File.separator+"pmis"+File.separator+"ISSUE_FILES"+File.separator;
-	public static final String SAFETY_FILE_SAVING_PATH = "E:"+File.separator+"Development"+File.separator+"nginx-1.9.9"+File.separator+"html"+File.separator+"pmis"+File.separator+"SAFETY_FILES"+File.separator;;
+	public static final String SAFETY_FILE_SAVING_PATH = "E:"+File.separator+"Development"+File.separator+"nginx-1.9.9"+File.separator+"html"+File.separator+"pmis"+File.separator+"SAFETY_FILES"+File.separator;
+
+	/*********** PRODUCTION ****************************************************/
+	
+	/*public final static String ISSUE_FILE_SAVING_PATH = "usr/share/nginx/html/pmis/ISSUE_FILES/";
+	public final static String SAFETY_FILE_SAVING_PATH = "usr/share/nginx/html/pmis/SAFETY_FILES/";*/
 }

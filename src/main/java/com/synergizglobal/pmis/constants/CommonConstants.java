81cff568fdd2fdeb73c950eb50815a8ebe23956d
nihalswarna <nihal.swarna@synergizglobal.com>
2020-11-27 11:26:39 +0530

    tableau trusted ticket

--- src/main/java/com/synergizglobal/pmis/constants/CommonConstants.java
+++ src/main/java/com/synergizglobal/pmis/constants/CommonConstants.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class CommonConstants {
 	//public final static String BASE_URL = "https://infoviz.syntrackpro.com/trusted/{0}/";  //{0} is the placeholder for tableau trusted token
-	public final static String BASE_URL = "http://10.203.10.157:8000/trusted/trusted/{0}/";  //{0} is the placeholder for tableau trusted token
+	public final static String BASE_URL = "http://10.203.10.157:8000/trusted/{0}/";  //{0} is the placeholder for tableau trusted token
 	public final static String TABLEAU_PARAMS = "&:tabs=no&:toolbar=no";
 	
 	

945fd0c040dddd59e43c090a198824fae74b190d
nihalswarna <nihal.swarna@synergizglobal.com>
2020-12-30 09:50:24 +0530

    alerts template

--- src/main/java/com/synergizglobal/pmis/Iservice/AlertsService.java
+++ src/main/java/com/synergizglobal/pmis/Iservice/AlertsService.java
@@ -1,7 +1,13 @@
 package com.synergizglobal.pmis.Iservice;
 
+import java.util.List;
+
+import com.synergizglobal.pmis.model.Alerts;
+
 public interface AlertsService {
 
 	boolean generateAtertsByCronJob() throws Exception;
+
+	List<Alerts> getAlertsList() throws Exception;
 	
 }

8415fb384fcd061a763619378a00997f0f080acc
nihalswarna <nihal.swarna@synergizglobal.com>
2021-01-08 17:42:21 +0530

    design

--- src/main/java/com/synergizglobal/pmis/IMPLdao/DesignDaoImpl.java
+++ src/main/java/com/synergizglobal/pmis/IMPLdao/DesignDaoImpl.java
@@ -89,7 +89,7 @@ public class DesignDaoImpl implements DesignDao{
 				qry = qry + " and drawing_type_fk = ?";
 				arrSize++;
 			}
-			qry = qry + " limit 10";
+			//qry = qry + " limit 10";
 			Object[] pValues = new Object[arrSize];
 			int i = 0;
 			if(!StringUtils.isEmpty(obj) && !StringUtils.isEmpty(obj.getWork_id_fk())) {

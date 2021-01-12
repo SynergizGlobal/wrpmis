1838550f2918b4dd6fc62555d7841478d4d083af
nihalswarna <nihal.swarna@synergizglobal.com>
2021-01-12 11:23:26 +0530

    alerts

--- src/main/java/com/synergizglobal/pmis/IMPLdao/AlertsDaoImpl.java
+++ src/main/java/com/synergizglobal/pmis/IMPLdao/AlertsDaoImpl.java
@@ -170,30 +170,44 @@ public class AlertsDaoImpl implements AlertsDao{
 			}
 			
 			/***************************** Contract Value alerts*******************************************************/
-			String cvQryAlert1 = "select contract_id,'1st Alert' as alert_level,'Contract Value' as alert_type,CONCAT('Cumulative expenditure : ',cumulative_expenditure, ', Revised cost : ',revised_cost ) AS alert_value,hod_email,dy_hod_email"
+			String cvQryAlert1 = "select contract_id,'1st Alert' as alert_level,'Contract Value' as alert_type,CONCAT('Cumulative expenditure : ',cumulative_expenditure,"
+					+ "(CASE WHEN revised_cost is not null THEN ', Revised Cost : ' WHEN estimated_cost is not null THEN ', Estimated Cost : ' WHEN awarded_cost is not null THEN ', Awarded Cost : ' ELSE '' END)," + 
+					"(CASE WHEN revised_cost is not null THEN revised_cost WHEN estimated_cost is not null THEN estimated_cost WHEN awarded_cost is not null THEN awarded_cost ELSE 0 END)"
+					+ ") AS alert_value,hod_email,dy_hod_email"
 					+ " from contract_view " + 
 					"where contract_status = 'In Progress' " + 
-					"and ((cumulative_expenditure is not null and revised_cost is not null and completed_cost is null and ((revised_cost * 100) / cumulative_expenditure) > 95 and ((revised_cost * 100) / cumulative_expenditure) <= 120) or (cumulative_expenditure is not null and completed_cost is not null and revised_cost is null and ((completed_cost * 100) / cumulative_expenditure) > 95 and ((completed_cost * 100) / cumulative_expenditure) <= 120))";
+					"and ((cumulative_expenditure is not null "
+					+ "and (( cumulative_expenditure* 100) / (CASE WHEN revised_cost is not null THEN revised_cost WHEN estimated_cost is not null THEN estimated_cost WHEN awarded_cost is not null THEN awarded_cost ELSE 0 END)) >= 95 "
+					+ "and (( cumulative_expenditure* 100) / (CASE WHEN revised_cost is not null THEN revised_cost WHEN estimated_cost is not null THEN estimated_cost WHEN awarded_cost is not null THEN awarded_cost ELSE 0 END)) < 120))";
 			
 			List<Alerts> cvQryAlert1List = jdbcTemplate.query( cvQryAlert1, new BeanPropertyRowMapper<Alerts>(Alerts.class));
 			if(!StringUtils.isEmpty(cvQryAlert1List) && cvQryAlert1List.size() > 0) {
 				list.addAll(cvQryAlert1List);
 			}
 			
-			String cvQryAlert2 = "select contract_id,'2nd Alert' as alert_level,'Contract Value' as alert_type,CONCAT('Cumulative expenditure : ',cumulative_expenditure, ', Revised cost : ',revised_cost ) AS alert_value,hod_email,dy_hod_email"
+			String cvQryAlert2 = "select contract_id,'2nd Alert' as alert_level,'Contract Value' as alert_type,CONCAT('Cumulative expenditure : ',cumulative_expenditure,"
+					+ "(CASE WHEN revised_cost is not null THEN ', Revised Cost : ' WHEN estimated_cost is not null THEN ', Estimated Cost : ' WHEN awarded_cost is not null THEN ', Awarded Cost : ' ELSE '' END)," + 
+					"(CASE WHEN revised_cost is not null THEN revised_cost WHEN estimated_cost is not null THEN estimated_cost WHEN awarded_cost is not null THEN awarded_cost ELSE 0 END)"
+					+ " ) AS alert_value,hod_email,dy_hod_email"
 					+ " from contract_view " + 
 					"where contract_status = 'In Progress' " + 
-					"and ((cumulative_expenditure is not null and revised_cost is not null and completed_cost is null and ((revised_cost * 100) / cumulative_expenditure) > 120  and ((revised_cost * 100) / cumulative_expenditure) <= 145) or (cumulative_expenditure is not null and completed_cost is not null and revised_cost is null and ((completed_cost * 100) / cumulative_expenditure) >= 120 and ((completed_cost * 100) / cumulative_expenditure) <= 145))";
+					"and ((cumulative_expenditure is not null "
+					+ "and (( cumulative_expenditure* 100) / (CASE WHEN revised_cost is not null THEN revised_cost WHEN estimated_cost is not null THEN estimated_cost WHEN awarded_cost is not null THEN awarded_cost ELSE 0 END)) >= 120 "
+					+ "and (( cumulative_expenditure* 100) / (CASE WHEN revised_cost is not null THEN revised_cost WHEN estimated_cost is not null THEN estimated_cost WHEN awarded_cost is not null THEN awarded_cost ELSE 0 END)) < 145))";
 			
 			List<Alerts> cvQryAlert2List = jdbcTemplate.query( cvQryAlert2, new BeanPropertyRowMapper<Alerts>(Alerts.class));
 			if(!StringUtils.isEmpty(cvQryAlert2List) && cvQryAlert2List.size() > 0) {
 				list.addAll(cvQryAlert2List);
 			}
 			
-			String cvQryAlert3 = "select contract_id,'3rd Alert' as alert_level,'Contract Value' as alert_type,CONCAT('Cumulative expenditure : ',cumulative_expenditure, ', Revised cost : ',revised_cost ) AS alert_value,hod_email,dy_hod_email"
+			String cvQryAlert3 = "select contract_id,'3rd Alert' as alert_level,'Contract Value' as alert_type,CONCAT('Cumulative expenditure : ',cumulative_expenditure,"
+					+ "(CASE WHEN revised_cost is not null THEN ', Revised Cost : ' WHEN estimated_cost is not null THEN ', Estimated Cost : ' WHEN awarded_cost is not null THEN ', Awarded Cost : ' ELSE '' END)," + 
+					"(CASE WHEN revised_cost is not null THEN revised_cost WHEN estimated_cost is not null THEN estimated_cost WHEN awarded_cost is not null THEN awarded_cost ELSE 0 END)"
+					+ " ) AS alert_value,hod_email,dy_hod_email"
 					+ " from contract_view " + 
 					"where contract_status = 'In Progress' " + 
-					"and ((cumulative_expenditure is not null and revised_cost is not null and completed_cost is null and ((revised_cost * 100) / cumulative_expenditure) > 145) or (cumulative_expenditure is not null and completed_cost is not null and revised_cost is null and ((completed_cost * 100) / cumulative_expenditure) > 145))";
+					"and ((cumulative_expenditure is not null "
+					+ "and (( cumulative_expenditure* 100) / (CASE WHEN revised_cost is not null THEN revised_cost WHEN estimated_cost is not null THEN estimated_cost WHEN awarded_cost is not null THEN awarded_cost ELSE 0 END)) >= 145))";
 			
 			List<Alerts> cvQryAlert3List = jdbcTemplate.query( cvQryAlert3, new BeanPropertyRowMapper<Alerts>(Alerts.class));
 			if(!StringUtils.isEmpty(cvQryAlert3List) && cvQryAlert3List.size() > 0) {

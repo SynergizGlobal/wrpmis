7d82c71011048cd0d11b956fe388528b3a724041
gurubig160 <kallurigurappa@gmail.com>
2021-04-05 10:11:49 +0530

    tableau

--- src/main/java/com/synergizglobal/pmis/controller/TableauDashboardController.java
+++ src/main/java/com/synergizglobal/pmis/controller/TableauDashboardController.java
@@ -191,7 +191,7 @@ public class TableauDashboardController {
 				String trustedTokenId =  tObj.getTrustedTicket();
 				CommonConstants cObj = new CommonConstants();
 				String baseUrl = cObj.BASE_URL.replace("{0}", trustedTokenId);
-				String tableauUrl = baseUrl + url[1]+"?issue_id="+issue_id+CommonConstants.TABLEAU_PARAMS;
+				String tableauUrl = baseUrl + url[1]+"?issue_id="+issue_id;
 				vo.setTableauUrl(tableauUrl);
 			}
 			view.addObject("url", vo);

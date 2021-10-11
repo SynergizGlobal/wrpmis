6ff6e6635c066a2e8dc330f6cb8b3a418a654e7c
Synergiz <Synergiz@DESKTOP-VI4L55V>
2021-10-11 16:44:33 +0530

    Committed Messages issues distinct

--- src/main/java/com/synergizglobal/pmis/IMPLdao/HomeDaoImpl.java
+++ src/main/java/com/synergizglobal/pmis/IMPLdao/HomeDaoImpl.java
@@ -880,16 +880,22 @@ public class HomeDaoImpl implements HomeDao {
 	public List<Messages> getMessages(Messages mObj) throws Exception {
 		List<Messages> objsList = null;
 		try {
-			String qry ="select message_id,message,user_id_fk,redirect_url,DATE_FORMAT(created_date,'%d-%m-%Y %h:%i %p') as created_date,created_date as created_date_24hr_format, "
+			/*String qry ="select message_id,message,user_id_fk,redirect_url,DATE_FORMAT(created_date,'%d-%m-%Y %h:%i %p') as created_date,created_date as created_date_24hr_format, "
+					+ "read_time,message_type "
+					+ "from messages where user_id_fk = ? "
+					+ "and (read_time is null or read_time > (NOW() - INTERVAL 3 DAY)) ";*/
+			
+			String qry ="select distinct message,user_id_fk,redirect_url,left(DATE_FORMAT(created_date,'%d-%m-%Y %h:%i %p'),10) as created_date,concat(left(created_date,10),' 00:00:00') as created_date_24hr_format, "
 					+ "read_time,message_type "
 					+ "from messages where user_id_fk = ? "
 					+ "and (read_time is null or read_time > (NOW() - INTERVAL 3 DAY)) ";
+			
 			int arrSize = 1;		
 			if(!StringUtils.isEmpty(mObj.getMessage_type())) {
 				qry = qry + "and message_type = ? ";
 				arrSize++;
 			}
-			qry = qry + "order by created_date_24hr_format desc,DATE_FORMAT(created_date_24hr_format,'%H:%i:%s') DESC";
+			qry = qry + "order by left(created_date_24hr_format,10) desc,left(DATE_FORMAT(created_date_24hr_format,'%H:%i:%s'),10) DESC";
 			Object[] pValues = new Object[arrSize];
 			int i = 0;
 			pValues[i++] = mObj.getUser_id_fk();

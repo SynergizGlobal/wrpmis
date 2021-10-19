a4a29d6b7d49a07961fddf747afa6d7cc7055110
Synergiz <Synergiz@192.168.0.103>
2021-10-19 11:47:58 +0530

    Committing messages Unique

--- src/main/java/com/synergizglobal/pmis/IMPLdao/HomeDaoImpl.java
+++ src/main/java/com/synergizglobal/pmis/IMPLdao/HomeDaoImpl.java
@@ -883,24 +883,25 @@ public class HomeDaoImpl implements HomeDao {
 			String qry ="select message_id,message,user_id_fk,redirect_url,DATE_FORMAT(created_date,'%d-%m-%Y %h:%i %p') as created_date,created_date as created_date_24hr_format, "
 					+ "read_time,message_type "
 					+ "from messages where user_id_fk = ? "
-					+ "and (read_time is null or read_time > (NOW() - INTERVAL 3 DAY)) ";
-			
-			/*String qry ="select distinct message,user_id_fk,redirect_url,left(DATE_FORMAT(created_date,'%d-%m-%Y %h:%i %p'),10) as created_date,"
+					+ "and (read_time is null or read_time > (NOW() - INTERVAL 3 DAY)) and message_type not in ('Risk') union all "
+				    + " select distinct (SELECT MAX(message_id) FROM messages m where m.redirect_url=redirect_url and left(m.created_date,10)=left(m2.created_date,10) "
+				    + "and m.message_type=m2.message_type) as message_id,message,user_id_fk,redirect_url,left(DATE_FORMAT(created_date,'%d-%m-%Y %h:%i %p'),10) as created_date,"
 					+ "(SELECT MAX(created_date) FROM messages m where m.redirect_url=redirect_url and left(m.created_date,10)=left(m2.created_date,10) "
 					+ "and m.message_type=m2.message_type"
 					+ ") as created_date_24hr_format, "
 					+ "read_time,message_type "
 					+ "from messages m2 where user_id_fk = ? "
-					+ "and (read_time is null or read_time > (NOW() - INTERVAL 3 DAY)) ";*/
+					+ "and (read_time is null or read_time > (NOW() - INTERVAL 3 DAY)) and message_type in ('Risk')";
 			
-			int arrSize = 1;		
+			int arrSize = 2;		
 			if(!StringUtils.isEmpty(mObj.getMessage_type())) {
 				qry = qry + "and message_type = ? ";
 				arrSize++;
 			}
-			qry = qry + "order by created_date_24hr_format desc,DATE_FORMAT(created_date_24hr_format,'%H:%i:%s') DESC";
+			//qry = qry + "order by created_date_24hr_format desc,DATE_FORMAT(created_date_24hr_format,'%H:%i:%s') DESC";
 			Object[] pValues = new Object[arrSize];
 			int i = 0;
+			pValues[i++] = mObj.getUser_id_fk();
 			pValues[i++] = mObj.getUser_id_fk();
 			if(!StringUtils.isEmpty(mObj.getMessage_type())) {
 				pValues[i++] = mObj.getMessage_type();

619654c42ff4dfbbcc5961cadc295788924414f2
Synergiz <Synergiz@DESKTOP-L6C2KOM>
2021-10-27 16:33:17 +0530

    Template upload change finished

--- src/main/java/com/synergizglobal/pmis/controller/TemplateUploadController.java
+++ src/main/java/com/synergizglobal/pmis/controller/TemplateUploadController.java
@@ -4,10 +4,14 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
 
 import javax.servlet.http.HttpSession;
 
@@ -27,6 +31,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.synergizglobal.pmis.Iservice.TemplateUploadService;
 import com.synergizglobal.pmis.common.FileUploads;
+import com.synergizglobal.pmis.common.OSValidator;
 import com.synergizglobal.pmis.constants.CommonConstants;
 import com.synergizglobal.pmis.constants.PageConstants;
 import com.synergizglobal.pmis.reference.model.TrainingType;
@@ -74,7 +79,7 @@ public class TemplateUploadController {
 			String timeStamp = new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime());
 			String timeStamp_for_file = new SimpleDateFormat("yyyy-MM-dd_HH_MM_SS").format(Calendar.getInstance().getTime());
 			obj.setUploaded_on(timeStamp);
-			MultipartFile file = obj.getTemplateFile();
+			
 			File theDir = new File(CommonConstants.TEMPLATE_OLD_FILEPATH);
 			if (!theDir.exists()){
 			    theDir.mkdirs();
@@ -82,11 +87,8 @@ public class TemplateUploadController {
 			String commonAttachment = obj.getCommonAttachment();
 			Path oldFile  = Paths.get(CommonConstants.TEMPLATE_FILEPATH + obj.getTemplate_name()+".xlsx");
 			String renameFile = obj.getTemplate_name() +"_"+ timeStamp_for_file+"_"+renNum+renNum2+".xlsx";
-			Files.move(oldFile, oldFile.resolveSibling(CommonConstants.TEMPLATE_FILEPATH + renameFile));
-			
-			Path temp = Files.move
-			        (Paths.get(CommonConstants.TEMPLATE_FILEPATH + renameFile), 
-			        Paths.get(CommonConstants.TEMPLATE_OLD_FILEPATH + renameFile));
+			Path renamingExistingFileToNewName = Files.move(oldFile, oldFile.resolveSibling(CommonConstants.TEMPLATE_FILEPATH + renameFile));
+			MultipartFile file = obj.getTemplateFile();
 			String fileDirectory = CommonConstants.TEMPLATE_FILEPATH ;
 			
 			String fileName = file.getOriginalFilename();
@@ -95,6 +97,44 @@ public class TemplateUploadController {
 			obj.setCommonAttachment(fileName_new);
 			FileUploads.singleFileSaving(file, fileDirectory, fileName_new);
 			
+			
+			//FileUploads.singleFileSaving(file, CommonConstants.TEMPLATE_OLD_FILEPATH, renameFile);
+			boolean isUnix = OSValidator.isUnix();
+        	if(isUnix) {
+            	String perm = "rwxrwxrwx";
+            	Set<PosixFilePermission> permissions = PosixFilePermissions.fromString(perm);
+            	Files.setPosixFilePermissions(theDir.toPath(), permissions);
+        	}
+			Path movingExistingFileToNewPath = Files.move
+			        (Paths.get(CommonConstants.TEMPLATE_FILEPATH + renameFile), 
+			        Paths.get(CommonConstants.TEMPLATE_OLD_FILEPATH + renameFile));
+			File old_file = new File(theDir + renameFile);
+                if(old_file.exists()) {
+                    //Setting file permissions for owner, group and others using PosixFilePermission
+                     
+                    HashSet<PosixFilePermission> permissions = new HashSet<PosixFilePermission>();
+                     
+                    //Adding owner's file permissions
+                     
+                    permissions.add(PosixFilePermission.OWNER_EXECUTE);
+                    permissions.add(PosixFilePermission.OWNER_READ);
+                    permissions.add(PosixFilePermission.OWNER_WRITE);
+                     
+                    //Adding group's file permissions
+                     
+                    permissions.add(PosixFilePermission.GROUP_EXECUTE);
+                    permissions.add(PosixFilePermission.GROUP_READ);
+                    permissions.add(PosixFilePermission.GROUP_WRITE);
+                     
+                    //Adding other's file permissions
+                     
+                    permissions.add(PosixFilePermission.OTHERS_EXECUTE);
+                    permissions.add(PosixFilePermission.OTHERS_READ);
+                    permissions.add(PosixFilePermission.OTHERS_WRITE);
+                     
+                    Files.setPosixFilePermissions(Paths.get(theDir + renameFile), permissions);
+                }
+        	
 			boolean flag =  service.uploadTemplate(obj);
 			//boolean old = setOldFilePath(obj,session,attributes);
 			if(flag) {

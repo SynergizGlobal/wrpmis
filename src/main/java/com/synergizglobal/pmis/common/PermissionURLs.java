8c29445dbe810e1199a3f106eb1dc8cfd9019734
gurubig160 <kallurigurappa@gmail.com>
2021-12-17 14:21:06 +0530

    log4j velnerability

--- src/main/java/com/synergizglobal/pmis/common/PermissionURLs.java
+++ src/main/java/com/synergizglobal/pmis/common/PermissionURLs.java
@@ -20,6 +20,22 @@ public class PermissionURLs {
 	    "/pmis/reset-password",
 	    "/reset-password",
 	    "/pmis/change-password",
-	    "/change-password"
+	    "/change-password",
+	    "/update-user-manual", 
+	    "/add-user-manual", 
+	    "/delete-user-manual", 
+	    "/user-manuals", 
+	    "/update-web-document/codes-&-manuals", 
+	    "/upload-web-document/codes-&-manuals", 
+	    "/delete-web-document/codes-&-manuals", 
+	    "/web-documents/codes-&-manuals", 
+	    "/update-web-document/policies-&-circulars", 
+	    "/upload-web-document/policies-&-circulars", 
+	    "/delete-web-document/policies-&-circulars", 
+	    "/web-documents/policies-&-circulars", 
+	    "/web-document-types",
+	    "/fobmulti-gallery/{fob_id}", 
+	    "/fob-gallery/{fob_id}", 
+	    "/auto-responsibility"
 	);
 }

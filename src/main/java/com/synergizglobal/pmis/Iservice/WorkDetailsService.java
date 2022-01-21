852d46c794123b909edd7e6c6233b0937c82296d
gurubig160 <kallurigurappa@gmail.com>
2022-01-21 12:45:11 +0530

    work details

---
+++ src/main/java/com/synergizglobal/pmis/Iservice/WorkDetailsService.java
@@ -0,0 +1,36 @@
+package com.synergizglobal.pmis.Iservice;
+
+import java.util.List;
+
+import com.synergizglobal.pmis.model.WorkFeatures;
+
+public interface WorkDetailsService {
+
+
+	public List<WorkFeatures> getWorkDetailsList(WorkFeatures obj) throws Exception;
+	
+	public List<WorkFeatures> getWorkDetails(WorkFeatures obj) throws Exception;
+
+	public boolean addWorkDetails(WorkFeatures obj) throws Exception;
+
+	public boolean updateWorkDetails(WorkFeatures obj) throws Exception;
+
+	public List<WorkFeatures> getWorksListFilter(WorkFeatures obj) throws Exception;
+
+	public List<WorkFeatures> getTitlesList(WorkFeatures obj) throws Exception;
+
+	public List<WorkFeatures> getStatusList(WorkFeatures obj) throws Exception;
+
+	public List<WorkFeatures> getWorksList(WorkFeatures obj) throws Exception;
+
+	public List<WorkFeatures> getWorksListFilterForSalientFeatures(WorkFeatures obj) throws Exception;
+
+	public List<WorkFeatures> getWorkSalientFeatures(WorkFeatures obj) throws Exception;
+
+	public List<WorkFeatures> getCategoryList(WorkFeatures obj) throws Exception;
+
+	public boolean addWorkSalientFeatures(WorkFeatures obj) throws Exception;
+
+	public boolean updateWorkSalientFeatures(WorkFeatures obj) throws Exception;
+	
+}

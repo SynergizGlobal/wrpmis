5fd7341596b2d9da0712dea8dc266d6e171778fa
gurubig160 <kallurigurappa@gmail.com>
2022-04-30 16:09:34 +0530

    cron jobs

--- src/main/java/com/synergizglobal/pmis/controller/Schedular.java
+++ src/main/java/com/synergizglobal/pmis/controller/Schedular.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.InitBinder;
 
 import com.synergizglobal.pmis.Iservice.AlertsService;
 import com.synergizglobal.pmis.Iservice.HomeService;
+import com.synergizglobal.pmis.common.UrlGenerator;
 import com.synergizglobal.pmis.constants.CommonConstants2;
 import com.synergizglobal.pmis.model.Issue;
 import com.synergizglobal.pmis.model.User;
@@ -49,154 +50,171 @@ public class Schedular {
 	@Value("${common.error.message}")
 	public String commonError;
 	
+	UrlGenerator ugObj = new UrlGenerator();
+	public boolean is_cron_jobs_enabled = ugObj.getIsCronJobsEnbled();
+	
 	
 	/**********************************************************************************/
 	
-	@Scheduled(cron = "${${cron.schedular.env}.cron.expression.user.login.timeout}")
+	@Scheduled(cron = "${cron.expression.user.login.timeout}")
 	public void userLoginTimeout(){
-	     try {
-	    	 //logger.error("userLoginTimeout : Method executed at > "+new Date());
-             boolean flag = homeService.userLoginTimeout();
-	    	 //logger.error("userLoginTimeout : "+flag);
-		 } catch (Exception e) {
-			 e.printStackTrace();
-			 logger.error("userLoginTimeout() : "+e.getMessage());
-		 }
+		if(is_cron_jobs_enabled) {
+			try {
+		    	 //logger.error("userLoginTimeout : Method executed at > "+new Date());
+	             boolean flag = homeService.userLoginTimeout();
+		    	 //logger.error("userLoginTimeout : "+flag);
+			 } catch (Exception e) {
+				 e.printStackTrace();
+				 logger.error("userLoginTimeout() : "+e.getMessage());
+			 }
+		}
 	}
 	/**********************************************************************************/	
 	
-	@Scheduled(cron = "${${cron.schedular.env}.cron.expression.generate.alerts}")
+	@Scheduled(cron = "${cron.expression.generate.alerts}")
 	public void generateAlertsByCronJob(){	
-	     logger.error("generateAlertsByCronJob : Method executed every day. Current time is :"+ new Date());	    
-	     try {
-	    	 
-	    	//System.out.println("Start "+ new Date());
-            boolean flag = alertService.generateAterts();
-            //System.out.println("End "+ new Date());
-	    	logger.error("generateAlertsByCronJob : "+flag);
-	    	
-	    	flag = alertService.sendEMailNotificationWithRiskAlerts();
-			logger.error("sendEMailNotificationWithRiskAlerts >> Sent mails : "+ flag); 
-			
-			flag = alertService.sendEMailNotificationAlertsToITAdmins();
-			logger.error("sendEMailNotificationAlertsToITAdmins >> Sent mails : "+ flag); 
-			
-			//Calling stored procedures
-			flag = alertService.callingStoredProcedures();
-			logger.error("callingStoredProcedures >> Run Procedures : "+ flag);
-			
-		 } catch (Exception e) {
-			 e.printStackTrace();
-			logger.error("generateAlertsByCronJob() : "+e.getMessage());
-		 }
+		if(is_cron_jobs_enabled) {
+		     logger.error("generateAlertsByCronJob : Method executed every day. Current time is :"+ new Date());	    
+		     try {
+		    	 
+		    	//System.out.println("Start "+ new Date());
+	            boolean flag = alertService.generateAterts();
+	            //System.out.println("End "+ new Date());
+		    	logger.error("generateAlertsByCronJob : "+flag);
+		    	
+		    	flag = alertService.sendEMailNotificationWithRiskAlerts();
+				logger.error("sendEMailNotificationWithRiskAlerts >> Sent mails : "+ flag); 
+				
+				flag = alertService.sendEMailNotificationAlertsToITAdmins();
+				logger.error("sendEMailNotificationAlertsToITAdmins >> Sent mails : "+ flag); 
+				
+				//Calling stored procedures
+				flag = alertService.callingStoredProcedures();
+				logger.error("callingStoredProcedures >> Run Procedures : "+ flag);
+				
+			 } catch (Exception e) {
+				 e.printStackTrace();
+				logger.error("generateAlertsByCronJob() : "+e.getMessage());
+			 }
+		}
 	}
 	
 	
-	@Scheduled(cron = "${${cron.schedular.env}.cron.expression.generate.assign.responsibility}")
+	@Scheduled(cron = "${cron.expression.generate.assign.responsibility}")
 	public void generateAutoResponsibilityByCronJob(){	
-	     logger.error("generateAutoResponsibilityByCronJob : Method executed every day. Current time is :"+ new Date());	    
-	     try {
-	    	 
-	    	 logger.error("generateAutoResponsibilityByCronJob> "+new Date());
-	    	 loginController.autoResponsibility();
-		 } catch (Exception e) {
-			 e.printStackTrace();
-			logger.error("generateAutoResponsibilityByCronJob() : "+e.getMessage());
-		 }
+		if(is_cron_jobs_enabled) {
+		     logger.error("generateAutoResponsibilityByCronJob : Method executed every day. Current time is :"+ new Date());	    
+		     try {
+		    	 
+		    	 logger.error("generateAutoResponsibilityByCronJob> "+new Date());
+		    	 loginController.autoResponsibility();
+			 } catch (Exception e) {
+				 e.printStackTrace();
+				logger.error("generateAutoResponsibilityByCronJob() : "+e.getMessage());
+			 }
+		}
 	}	
 	
-	@Scheduled(cron = "${${cron.schedular.env}.cron.expression.sending.alert.mails.by.alert.type}")
-	public void sendNotificationAlertMailsToAllByCronJob(){		   
-	     logger.error("sendNotificationAlertMailsToAllByCronJob : Current time is :"+ new Date());	    
-	     try {
-	    	   Date date = new Date();
-			   Calendar cal = Calendar.getInstance();
-	           cal.setTime(date); // don't forget this if date is arbitrary
-	             
-	           SimpleDateFormat monthFormat = new SimpleDateFormat("MMMM");
-	           SimpleDateFormat dayOfWeekTextFormat = new SimpleDateFormat("EEEE");
-	           String dayOfWeekText = dayOfWeekTextFormat.format(date).toUpperCase();
-	           //int month = cal.get(Calendar.MONTH); // 0 being January
-	           
-				String alert_type = null;
-				if(dayOfWeekText.equals("MONDAY")) {
-				   alert_type = CommonConstants2.ALERT_TYPE_BANK_GUARANTEE;
-				}else if(dayOfWeekText.equals("TUESDAY")) {
-				   alert_type = CommonConstants2.ALERT_TYPE_INSURANCE;
-				}else if(dayOfWeekText.equals("WEDNESDAY")) {
-				   alert_type = CommonConstants2.ALERT_TYPE_CONTRACT_PERIOD;
-				}else if(dayOfWeekText.equals("THURSDAY")) {
-				   alert_type = CommonConstants2.ALERT_TYPE_CONTRACT_VALUE;
-				}else if(dayOfWeekText.equals("FRIDAY")) {
-				   alert_type = CommonConstants2.ALERT_TYPE_ISSUE;
-				}
-				
-				if(!StringUtils.isEmpty(alert_type)) {
-					if(dayOfWeekText.equals("FRIDAY")) {
-					    boolean flag = alertService.sendEMailNotificationWithIssueAlerts(alert_type);
-					    logger.error("sendEMailNotificationWithIssueAlertsByCronJob >> Sent mails : "+ flag);
-					}else {
-						boolean flag = alertService.sendEMailNotificationWithContractAlerts(alert_type);
-						logger.error("sendEMailNotificationWithContractAlertsByCronJob >> "+alert_type+" Sent mails : "+ flag);
+	@Scheduled(cron = "${cron.expression.sending.alert.mails.by.alert.type}")
+	public void sendNotificationAlertMailsToAllByCronJob(){		
+		if(is_cron_jobs_enabled) {
+		     logger.error("sendNotificationAlertMailsToAllByCronJob : Current time is :"+ new Date());	    
+		     try {
+		    	   Date date = new Date();
+				   Calendar cal = Calendar.getInstance();
+		           cal.setTime(date); // don't forget this if date is arbitrary
+		             
+		           SimpleDateFormat monthFormat = new SimpleDateFormat("MMMM");
+		           SimpleDateFormat dayOfWeekTextFormat = new SimpleDateFormat("EEEE");
+		           String dayOfWeekText = dayOfWeekTextFormat.format(date).toUpperCase();
+		           //int month = cal.get(Calendar.MONTH); // 0 being January
+		           
+					String alert_type = null;
+					if(dayOfWeekText.equals("MONDAY")) {
+					   alert_type = CommonConstants2.ALERT_TYPE_BANK_GUARANTEE;
+					}else if(dayOfWeekText.equals("TUESDAY")) {
+					   alert_type = CommonConstants2.ALERT_TYPE_INSURANCE;
+					}else if(dayOfWeekText.equals("WEDNESDAY")) {
+					   alert_type = CommonConstants2.ALERT_TYPE_CONTRACT_PERIOD;
+					}else if(dayOfWeekText.equals("THURSDAY")) {
+					   alert_type = CommonConstants2.ALERT_TYPE_CONTRACT_VALUE;
+					}else if(dayOfWeekText.equals("FRIDAY")) {
+					   alert_type = CommonConstants2.ALERT_TYPE_ISSUE;
 					}
-				}
-	           
-				/*String alert_type = null;
-				if(dayOfWeekText.equals("WEDNESDAY")) {
-				   alert_type = CommonConstants2.ALERT_TYPE_CONTRACT;
-				   boolean flag = alertService.sendEMailNotificationWithContractAlerts(alert_type);
-				   logger.error("sendEMailNotificationWithContractAlertsByCronJob >> Sent mails : "+ flag);
-				}else if(dayOfWeekText.equals("FRIDAY")) {
-				   alert_type = CommonConstants2.ALERT_TYPE_ISSUE;
-				   boolean flag = alertService.sendEMailNotificationWithIssueAlerts(alert_type);
-				   logger.error("sendEMailNotificationWithIssueAlertsByCronJob >> Sent mails : "+ flag);
-				}*/
-			   
-		 } catch (Exception e) {
-			 e.printStackTrace();
-			logger.error("sendNotificationAlertMailsToAllByCronJob() : "+e.getMessage());
-		 }
+					
+					if(!StringUtils.isEmpty(alert_type)) {
+						if(dayOfWeekText.equals("FRIDAY")) {
+						    boolean flag = alertService.sendEMailNotificationWithIssueAlerts(alert_type);
+						    logger.error("sendEMailNotificationWithIssueAlertsByCronJob >> Sent mails : "+ flag);
+						}else {
+							boolean flag = alertService.sendEMailNotificationWithContractAlerts(alert_type);
+							logger.error("sendEMailNotificationWithContractAlertsByCronJob >> "+alert_type+" Sent mails : "+ flag);
+						}
+					}
+		           
+					/*String alert_type = null;
+					if(dayOfWeekText.equals("WEDNESDAY")) {
+					   alert_type = CommonConstants2.ALERT_TYPE_CONTRACT;
+					   boolean flag = alertService.sendEMailNotificationWithContractAlerts(alert_type);
+					   logger.error("sendEMailNotificationWithContractAlertsByCronJob >> Sent mails : "+ flag);
+					}else if(dayOfWeekText.equals("FRIDAY")) {
+					   alert_type = CommonConstants2.ALERT_TYPE_ISSUE;
+					   boolean flag = alertService.sendEMailNotificationWithIssueAlerts(alert_type);
+					   logger.error("sendEMailNotificationWithIssueAlertsByCronJob >> Sent mails : "+ flag);
+					}*/
+				   
+			 } catch (Exception e) {
+				 e.printStackTrace();
+				logger.error("sendNotificationAlertMailsToAllByCronJob() : "+e.getMessage());
+			 }
+		}
 	}
 	
 	/******************************************************************************************/
 	
-	@Scheduled(cron = "${${cron.schedular.env}.cron.expression.send.mail.with.open.issues}")
+	@Scheduled(cron = "${cron.expression.send.mail.with.open.issues}")
 	public void sendMailWithOpenIssues(){
-	     try {
-	    	 logger.error("sendMailWithOpenIssues : Method executed at > "+new Date());
-	    	 Issue obj = new Issue();
-             boolean flag = issueReportController.sendMailWithOpenIssues(obj);
-	    	 logger.error("sendMailWithOpenIssues : "+flag);
-		 } catch (Exception e) {
-			 e.printStackTrace();
-			 logger.error("sendMailWithOpenIssues() : "+e.getMessage());
-		 }
+		if(is_cron_jobs_enabled) {
+		     try {
+		    	 logger.error("sendMailWithOpenIssues : Method executed at > "+new Date());
+		    	 Issue obj = new Issue();
+	             boolean flag = issueReportController.sendMailWithOpenIssues(obj);
+		    	 logger.error("sendMailWithOpenIssues : "+flag);
+			 } catch (Exception e) {
+				 e.printStackTrace();
+				 logger.error("sendMailWithOpenIssues() : "+e.getMessage());
+			 }
+		}
 	}
 	
-	@Scheduled(cron = "${${cron.schedular.env}.cron.expression.sending.user.login.report.mails}")
+	@Scheduled(cron = "${cron.expression.sending.user.login.report.mails}")
 	public void sendUserLoginReportByCronJob(){		
+		if(is_cron_jobs_enabled) {
 	     logger.error("sendUserLoginReportByCronJob : Current time is :"+ new Date());	    
-	     try {
-	    	 User uObj = new User();
-	    	 boolean isDownload = false;
-	    	 boolean flag = userLoginReportController.sendLast7DaysUserLoginDeatils(uObj,null,isDownload);
-		     logger.error("sendUserLoginReportByCronJob >> Sending mails : "+ flag);
-		     //System.out.println("Sending mails : "+ flag); 
-		 } catch (Exception e) {
-			 e.printStackTrace();
-			logger.error("sendUserLoginReportByCronJob() : "+e.getMessage());
-		 }
+		     try {
+		    	 User uObj = new User();
+		    	 boolean isDownload = false;
+		    	 boolean flag = userLoginReportController.sendLast7DaysUserLoginDeatils(uObj,null,isDownload);
+			     logger.error("sendUserLoginReportByCronJob >> Sending mails : "+ flag);
+			     //System.out.println("Sending mails : "+ flag); 
+			 } catch (Exception e) {
+				 e.printStackTrace();
+				logger.error("sendUserLoginReportByCronJob() : "+e.getMessage());
+			 }
+		}
 	}
 	
-	@Scheduled(cron = "${${cron.schedular.env}.cron.expression.send.mail.with.contract.bg.insurance.report}")
+	@Scheduled(cron = "${cron.expression.send.mail.with.contract.bg.insurance.report}")
 	public void sendMailWithContractBGInsuranceReport(){
-		try {
-	    	 logger.error("sendMailWithContractBGInsuranceReport : Method executed at > "+new Date());
-	    	 contractReportController.contractReportAutoEmail();
-	    	 logger.error("sendMailWithContractBGInsuranceReport : end");
-		} catch (Exception e) {
-			 e.printStackTrace();
-			 logger.error("sendMailWithContractBGInsuranceReport() : "+e.getMessage());
+		if(is_cron_jobs_enabled) {
+			try {
+		    	 logger.error("sendMailWithContractBGInsuranceReport : Method executed at > "+new Date());
+		    	 contractReportController.contractReportAutoEmail();
+		    	 logger.error("sendMailWithContractBGInsuranceReport : end");
+			} catch (Exception e) {
+				 e.printStackTrace();
+				 logger.error("sendMailWithContractBGInsuranceReport() : "+e.getMessage());
+			}
 		}
 	}
 		

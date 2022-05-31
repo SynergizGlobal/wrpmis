c2eb6e1affa360a7056829de5d293c282045cef8
Synergiz <Synergiz@DESKTOP-L6C2KOM>
2022-05-31 12:05:05 +0530

    Report

--- src/main/java/com/synergizglobal/pmis/IMPLdao/ActivitiesExportReportDaoImpl.java
+++ src/main/java/com/synergizglobal/pmis/IMPLdao/ActivitiesExportReportDaoImpl.java
@@ -166,15 +166,17 @@ public class ActivitiesExportReportDaoImpl implements ActivitiesExportReportDao{
 	public StripChart generateActivitiesExportReport(StripChart obj) throws Exception {
 		List<StripChart> objsList = null;
 		try {
-			String qry = "SELECT activity_id,  a.contract_id_fk,contract_short_name, from_structure_id, to_structure_id, structure_type_fk, section, line, a.structure, component, component_id, order_x, order_y, "
-					+ "activity_name, DATE_FORMAT(planned_start,'%d-%m-%Y') as planned_start, DATE_FORMAT(planned_finish,'%d-%m-%Y') as planned_finish, "
-					+ "DATE_FORMAT(actual_start,'%d-%m-%Y') as actual_start, DATE_FORMAT(actual_finish,'%d-%m-%Y') as actual_finish, a.unit, a.scope, a.completed, CAST(weightage AS DECIMAL(10,2)) as weightage, a.component_details, a.remarks, "
+			String qry = "SELECT a.activity_id, DATE_FORMAT(pcv.baseline_start,'%d-%m-%Y') as baseline_start, DATE_FORMAT(pcv.baseline_finish,'%d-%m-%Y') as baseline_finish,"
+					+ " DATE_FORMAT(pcv.start,'%d-%m-%Y') as start, DATE_FORMAT(pcv.finish,'%d-%m-%Y') as finish,pcv.status, a.contract_id_fk,contract_short_name, a.from_structure_id, a.to_structure_id, a.structure_type_fk, a.section, a.line, a.structure, a.component, a.component_id, a.order_x, a.order_y, "
+					+ "a.activity_name, DATE_FORMAT(a.planned_start,'%d-%m-%Y') as planned_start, DATE_FORMAT(a.planned_finish,'%d-%m-%Y') as planned_finish, "
+					+ "DATE_FORMAT(a.actual_start,'%d-%m-%Y') as actual_start, DATE_FORMAT(a.actual_finish,'%d-%m-%Y') as actual_finish, a.unit, a.scope, a.completed, CAST(a.weightage AS DECIMAL(10,2)) as weightage, a.component_details, a.remarks, "
 					+ "a.created_date, a.created_by_user_id_fk, a.modified_date, a.modified_by_user_id_fk, p6_task_code " + 
 					"from activities a "
+					+ "left join p6_contract_view pcv on pcv.contract_id = a.contract_id_fk and p6_task_code = pcv.task_code "
 					+ "LEFT JOIN contract c ON a.contract_id_fk = c.contract_id "
 					+ "left join work w on c.work_id_fk = w.work_id "
 					+ "left join project p on w.project_id_fk = p.project_id "
-					+ " where activity_id is not null and activity_id <> '' ";
+					+ " where a.activity_id is not null and a.activity_id <> '' ";
 			
 			int arrSize = 0;
 			
@@ -191,7 +193,7 @@ public class ActivitiesExportReportDaoImpl implements ActivitiesExportReportDao{
 				arrSize++;
 			}
 			
-			qry = qry + "  order by ISNULL(p6_task_code), p6_task_code ASC,activity_id";
+			qry = qry + "  order by ISNULL(p6_task_code), p6_task_code ASC,a.activity_id";
 			Object[] pValues = new Object[arrSize];
 			int i = 0;
 			if(!StringUtils.isEmpty(obj) && !StringUtils.isEmpty(obj.getContract_id_fk())) {

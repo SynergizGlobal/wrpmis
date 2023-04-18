158ef656e5ee4e8961c03fede1c8d7c4f1f2a5eb
Administrator <Administrator@192.168.0.104>
2023-04-18 15:27:39 +0530

    model file have changed

--- src/main/java/com/synergizglobal/pmis/common/TableauTrustedTicket.java
+++ src/main/java/com/synergizglobal/pmis/common/TableauTrustedTicket.java
@@ -3,111 +3,66 @@ package com.synergizglobal.pmis.common;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.InetAddress;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Enumeration;
+import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
 import org.apache.log4j.Logger;
+
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.JWSSigner;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+import java.util.Date;
 
 
 public class TableauTrustedTicket {
 	public static Logger logger = Logger.getLogger(TableauTrustedTicket.class);
 	public String getTrustedTicket(String server_name) throws Exception{
 		UrlGenerator ugObj = new UrlGenerator();
-		String getResponseString = "";
-		
-		/*String postURL = "https://infoviz.syntrackpro.com/trusted"; 
-		String username = "SynTrack"; 
-		String server = "infoviz.syntrackpro.com"; 
-		String clientIp = "www.syntrackpro.com";*/
-        
-		
-		/*String postURL = "http://"+ugObj.getIpAddress()+":8000/trusted"; 
-		String username = "SynTrack"; 
-		String server = ugObj.getIpAddress()+":8000";
-		String clientIp = ugObj.getIpAddress();*/
-		
-		String clientIpMap=getExternalIpAddress();
-		
-		String Str5[]=clientIpMap.split(".");
-		String Concat=Str5[2]+'.'+Str5[3];
-		
-		 InetAddress currentIPAddress;
-		 currentIPAddress = InetAddress.getLocalHost();
-		
-		 InetAddress IP=InetAddress.getLocalHost();
-		 String currentIPAddress1=IP.getHostAddress();		
 		
 		
-		String postURL = "";
-		String username = "";		
-		String server = ""; 
-		String clientIp = "";			
 		
-		 postURL = "http://203.153.40.44:8000/trusted";
-		 username = "SynTrack";		
-		 server = "203.153.40.44:8000"; 
+		String secret = "9PsDNdJzDp2GxN0t4/nr4EY24Vls6k3GbJIrSIWy7Tg=";
+		String kid = "8f1717ac-c5bd-4ec8-9edf-f32f2be48182";
+		String clientId = "38fac82e-7147-4f2d-8b21-aa0c264ec21d";
+		List<String> scopes = new
+	ArrayList<>(Arrays.asList("tableau:views:embed"));
+		String username = "SynTrack";
+		JWSSigner signer = new MACSigner(secret);
+		JWSHeader header = new
+	JWSHeader.Builder(JWSAlgorithm.HS256).keyID(kid).customParam("iss", clientId).build();
+		JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
+			.issuer(clientId)
+			.expirationTime(new Date(new Date().getTime() + 60 * 1000)) //expires in 1 minute
+			.jwtID(UUID.randomUUID().toString())
+			.audience("tableau")
+			.subject(username)
+			.claim("scp", scopes)
+			.build();
+		SignedJWT signedJWT = new SignedJWT(header, claimsSet);
+		signedJWT.sign(signer);
+		//System.out.println("token : "+ signedJWT.serialize());		
 		
-		/*postURL = "http://"+ugObj.getIpAddress()+":8000/trusted"; 
-		username = "SynTrack"; 
-		 server = ugObj.getIpAddress()+":8000";*/
-		 
-		 String Str[]=myPublicIp().split("___");
-		 String ipnew=Str[4];
-		 String Str1[]=ipnew.split(":");
-		 String ipnew1=Str1[1];	
-		 
-			String Str6[]=ipnew1.split(".");
-			String ConcatNew=Str6[0]+'.'+Str6[1]+'.'+Concat;
-			
-		 clientIp = ConcatNew;
-        
-        String target_site = "";//Optional
-		try {
-			HttpPost post = new HttpPost(postURL);
-
-			List<NameValuePair> params = new ArrayList<NameValuePair>();
-			params.add(new BasicNameValuePair("username", username));
-			params.add(new BasicNameValuePair("server", server));
-			params.add(new BasicNameValuePair("client_ip", clientIp));
-			params.add(new BasicNameValuePair("target_site", target_site));
-
-			UrlEncodedFormEntity ent = new UrlEncodedFormEntity(params, "UTF-8");
-			post.setEntity(ent);
-
-			HttpClient client = new DefaultHttpClient();
-			HttpResponse responsePOST = client.execute(post);
-			
-			
-			BufferedReader reader = new BufferedReader(new  InputStreamReader(responsePOST.getEntity().getContent()), 2048);
-
-			if (responsePOST != null) {
-			    StringBuilder sb = new StringBuilder();
-			    String line;
-			    while ((line = reader.readLine()) != null) {
-			        //System.out.println(" line : " + line);
-			        sb.append(line);
-			    }
-			    getResponseString = sb.toString();
-			    //use server output getResponseString as string value.
-			    
-			   // System.out.println("getResponseString : "+getResponseString);
-			}
-		} catch (Exception e) {
-			throw new Exception(e);
-		}
-		return getResponseString;
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+		return signedJWT.serialize();
 	}
 	
 	public String getExternalIpAddress() throws Exception {

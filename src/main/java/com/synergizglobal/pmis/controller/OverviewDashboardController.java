c2740873274d5d6879629bafe0728dc7223ae9f9
Administrator <Administrator@192.168.0.104>
2023-04-27 01:18:16 +0530

    ip address code have changed

--- src/main/java/com/synergizglobal/pmis/controller/OverviewDashboardController.java
+++ src/main/java/com/synergizglobal/pmis/controller/OverviewDashboardController.java
@@ -1,6 +1,10 @@
 package com.synergizglobal.pmis.controller;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.net.InetAddress;
+import java.net.URL;
 import java.net.URLDecoder;
 import java.sql.SQLException;
 import java.util.List;
@@ -354,7 +358,21 @@ public class OverviewDashboardController {
 						tableauUrl =  baseUrl +"/"+ url[1]+CommonConstants.TABLEAU_PARAMS+"&"+params;
 					}else {
 						tableauUrl =baseUrl +"/"+ url[1]+CommonConstants.TABLEAU_PARAMS;
-					}	
+					}
+					
+					InetAddress localhost = InetAddress.getLocalHost();
+			        System.out.println("System IP Address : " +
+			                      (localhost.getHostAddress()).trim());
+			        
+			        URL url_name = new URL("http://bot.whatismyipaddress.com");
+			        
+		            BufferedReader sc =
+		            new BufferedReader(new InputStreamReader(url_name.openStream()));
+		 
+		            // reads system IPAddress
+		            String systemipaddress = sc.readLine().trim();			        
+			        
+		            System.out.println("Public IP Address : " +systemipaddress);
 				obj.setDashboard_url(tableauUrl.toString());	
 			}
 		} catch (Exception e) {

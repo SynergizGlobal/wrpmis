b1b59bc15d42a42890ad81e291037da706bea1eb
Administrator <Administrator@192.168.0.100>
2023-12-14 15:52:28 +0530

    updated the query

--- src/main/java/com/synergizglobal/pmis/IMPLdao/DesignDaoImpl.java
+++ src/main/java/com/synergizglobal/pmis/IMPLdao/DesignDaoImpl.java
@@ -2396,12 +2396,11 @@ public class DesignDaoImpl implements DesignDao{
 	public List<Design> getP6ActivitiesData(Design obj) throws Exception {
 		List<Design> objsList = null;
 		try {
-			String qry ="select distinct task_code,p6_activity_id,s.structure,p.component,component_id as element,p6_activity_name as activity,p.scope,format(finish,'dd-MMM-yy') as target_date, " + 
+			String qry ="select distinct p.task_code,p6_activity_id,s.structure,p.component,component_id as element,p6_activity_name as activity,p.scope,format(finish,'dd-MMM-yy') as target_date, " + 
 					"dr.actual_date,dr.remarks from p6_activities p  " + 
 					"left join structure s on s.structure_id=p.structure_id_fk  " + 
-					"left join (select structure,component,element,activity,scope,target_date,actual_date,remarks from designdrawingstatusremarks where 0=0 and (actual_date!='' or remarks!='')) dr " + 
-					"on dr.structure=s.structure and dr.component=p.component and component_id=dr.element and p6_activity_name=dr.activity " + 
-					"and p.scope=dr.scope and format(finish,'dd-MMM-yy')=dr.target_date " + 
+					"left join (select structure,component,element,activity,scope,target_date,actual_date,remarks,task_code from designdrawingstatusremarks where 0=0 and (actual_date!='' or remarks!='')) dr " + 
+					"on dr.task_code=p.task_code  " + 
 					"where 0=0 and structure_type_fk = 'design' ";
 			int arrSize = 0;
 			if(!StringUtils.isEmpty(obj) && !StringUtils.isEmpty(obj.getContract_id_fk())) {
